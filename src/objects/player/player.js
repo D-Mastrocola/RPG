4d4fff1375f08902de22791314b48076b247e92a
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-07-22 14:12:08 -0400

    update

--- src/objects/player/player.js
+++ src/objects/player/player.js
@@ -12,18 +12,47 @@ class Player {
     };
     this.size = 30;
     this.canAttack = true;
+    this.isAttacking = false;
     this.objects = [];
+    this.sprites = {
+      up: loadImage('./src/objects/player/sprites/up.png'),
+      left: loadImage('./src/objects/player/sprites/left.png'),
+      right: loadImage('./src/objects/player/sprites/right.png'),
+      down: loadImage('./src/objects/player/sprites/down.png'),
+      upLeft: loadImage('./src/objects/player/sprites/upDiagonalLeft.png'),
+      upRight: loadImage('./src/objects/player/sprites/upDiagonalRight.png'),
+      downLeft: loadImage('./src/objects/player/sprites/downDiagonalLeft.png'),
+      downRight: loadImage('./src/objects/player/sprites/downDiagonalRight.png')
+    }
+    this.currentSprite = this.sprites.right;
   }
   attack() {
+    this.setVel();
     if (this.canAttack) {
+      this.isAttacking = true;
       this.canAttack = false;
       console.log("attack");
       let attack = new swordAttack(this.pos.x + this.dir.x * this.size, this.pos.y + this.dir.y * this.size);
       this.objects.push(attack);
-
+    }
+  }
+  setSprite() {
+    if(this.vel.y < 0) {
+      this.currentSprite = this.sprites.up;
+      if(this.vel.x > 0) this.currentSprite = this.sprites.upRight;
+      if(this.vel.x < 0) this.currentSprite = this.sprites.upLeft;
+    } else if(this.vel.y > 0) {
+      this.currentSprite = this.sprites.down;
+      if(this.vel.x > 0) this.currentSprite = this.sprites.downRight;
+      if(this.vel.x < 0) this.currentSprite = this.sprites.downLeft;
+    } else if(this.vel.x > 0) {
+      this.currentSprite = this.sprites.right;
+    } else if(this.vel.x < 0) {
+      this.currentSprite = this.sprites.left;
     }
   }
   setVel() {
+    this.vel.set(0, 0);
     let inputs = this.inputs;
     if (inputs.w === true) this.vel.add(0, -this.moveSpeed);
     if (inputs.a === true) this.vel.add(-this.moveSpeed, 0);
@@ -31,21 +60,23 @@ class Player {
     if (inputs.d === true) this.vel.add(this.moveSpeed, 0);
 
     this.vel.setMag(this.moveSpeed);
+    if(!this.isAttacking) this.setSprite();
   }
   draw() {
     fill(255);
     noStroke();
-    rect(this.pos.x, this.pos.y, this.size, this.size);
+    //rect(this.pos.x, this.pos.y, this.size, this.size);
+    image(this.currentSprite, this.pos.x, this.pos.y)
   }
   update() {
     if(this.vel.mag() !== 0) {
       this.dir = this.vel.copy().normalize();
     }
-    this.vel.set(0, 0);
     this.setVel();
     this.pos.add(this.vel);
     this.draw();
-
-    for(let i = 0; i < this.objects.length; i++) this.objects[i].update();
+    this.canAttack = true;
+    this.isAttacking = false;
+    for(let i = 0; i < this.objects.length; i++) this.objects[i].update(this.vel);
   }
 }
--- src/objects/player/swordAttack.js
+++ src/objects/player/swordAttack.js
@@ -1,13 +1,21 @@
-class swordAttack{
-    constructor(x, y) {
-        this.pos = createVector(x, y);
-        this.size = 30;
+class swordAttack {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.size = 30;
+    this.createdTime = millis();
+    this.length = 500;
+  }
+  draw() {
+    fill(255, 0, 0);
+    rect(this.pos.x, this.pos.y, this.size, this.size);
+  }
+  update(playerVel) {
+    let currentTime = millis();
+    if (currentTime - this.createdTime < this.length) {
+      this.pos.add(playerVel);
+      this.draw();
+      player.isAttacking = true;
+      player.canAttack = false;
     }
-    draw() {
-        fill(255, 0, 0);
-        rect(this.pos.x, this.pos.y, this.size, this.size)
-    }
-    update() {
-        this.draw();
-    }
+  }
 }
